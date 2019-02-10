a3c50d28c974da28ca0fb706111b25661344df50
Roman Morozov <hahahu91@gmail.com>
2019-02-10 17:54:20 +0300

    Программа для подсчета единичных битов в числе

---
+++ lab1/calcbits/calcbits/calcbits.cpp
@@ -0,0 +1,41 @@
+﻿// calcbits.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int bitcount(unsigned int x)
+{
+	int b;
+	for (b = 0; x != 0; x >>= 1)
+	{
+		if (x & 0x01)
+		{
+			++b;
+		}
+	}
+	return b;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Invalid arguments count\n"
+			 << "Usage: calcbits.exe <byte>\n";
+		return 1;
+	}
+
+	int byte = atoi(argv[1]);
+	if (byte > 0)
+	{
+		cout << bitcount(byte) << endl;
+	}
+	else
+	{
+		cout << "Invalid argument\n"
+			 << "Usage: calcbits.exe <positive number>\n";
+	}
+}
---
+++ lab1/calcbits/calcbits/test.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set PROGRAM="%~1"
+set OUT="%TEMP%\out.txt"
+
+
+%PROGRAM%  5 > %OUT%
+if ERRORLEVEL 1 goto err
+
+fc %OUT% 5out.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM%  15 > %OUT%
+if ERRORLEVEL 1 goto err
+
+fc %OUT% 15out.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM%  -15 > %OUT%
+if ERRORLEVEL 1 goto err
+
+fc %OUT% "-15out.txt"
+if ERRORLEVEL 1 goto err
+
+%PROGRAM%  Text > %OUT%
+if ERRORLEVEL 1 goto err
+
+fc %OUT% "Text-out.txt"
+if ERRORLEVEL 1 goto err
+
+echo Program test succesed
+exit /B 0
+
+:err
+echo Program test failed
+exit /B 1
