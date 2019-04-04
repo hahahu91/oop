4d727ce9d2e495e10875416e246927c25e932823
Roman Morozov <hahahu91@gmail.com>
2019-04-04 22:29:39 +0300

    remoteControlTests

--- lab3/TvSet-tests/remoteControlTests.cpp
+++ lab3/TvSet-tests/remoteControlTests.cpp
@@ -1,6 +1,68 @@
 #include "pch.h"
-#include "TvSet/TVSet.h"
-#include "TvSet/RemoteControl.h"
 
-SCENARIO("")
-{}
+#include "TvSet/RemoteControl.h"
+#include "TvSet/TVSet.h"
+
+#include <sstream>
+
+SCENARIO("Remote control can turn on a TV", "[remote]")
+{
+	GIVEN("A remote control connected to the TV which is turned off")
+	{
+		CTVSet tv;
+		std::stringstream input, output;
+		CRemoteControl rc(tv, input, output);
+
+		REQUIRE(!tv.IsTurnedOn());
+
+		WHEN("user enters TurnOn command")
+		{
+			input << "TurnOn";
+			CHECK(rc.HandleCommand());
+			THEN("tv switches on")
+			{
+				CHECK(tv.IsTurnedOn());
+				AND_THEN("user gets notification")
+				{
+					CHECK(output.str() == "TV is turned on\n");
+				}
+			}
+		}
+	}
+}
+
+SCENARIO("Remote control provides information about TV", "[remote]")
+{
+	CTVSet tv;
+	std::stringstream input;
+	std::ostringstream output;
+	input << "Info";
+	CRemoteControl rc(tv, input, output);
+	GIVEN("A turned off TV")
+	{
+		REQUIRE(!tv.IsTurnedOn());
+		WHEN("user enter Info command")
+		{
+			rc.HandleCommand();
+			THEN("it is notified that TV is off")
+			{
+				CHECK(output.str() == "TV is turned off\n");
+			}
+		}
+	}
+
+	GIVEN("A turned on TV")
+	{
+		tv.TurnOn();
+		tv.SelectChannel(17);
+		REQUIRE(tv.IsTurnedOn());
+		WHEN("user enter Info command")
+		{
+			rc.HandleCommand();
+			THEN("it is notified that TV is on the current channel")
+			{
+				CHECK(output.str() == "TV is turned on\nChannel is: 17\n");
+			}
+		}
+	}
+}
